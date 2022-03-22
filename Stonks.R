ae344567c48a9b91fc5421191a79d08dfa0ec869
khoody36 <khood@nevada.unr.edu>
2022-03-21 19:49:26 -0700

    Mr Chungs Magic Adventure

--- Stonks.R
+++ Stonks.R
@@ -1,5 +1,15 @@
-library(quantmod)
+library(pacman)
+pacman::p_load(data.table, fixest, BatchGetSymbols, finreportr, ggplot2, lubridate)
 
+first.date <- Sys.Date()-365
+last.date <- Sys.Date()
+freq.data <- "daily"
+df.SP500 <- GetSP500Stocks()
+tickers <- df.SP500$Tickers
 
-
-
+SP500stonks <- BatchGetSymbols(tickers = tickers,
+                               first.date = first.date,
+                               last.date = last.date,
+                               freq.data = freq.data,
+                               do.cache = FALSE,
+                               thresh.bad.data = 0)
